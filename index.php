25a2bbd682752aeab0c6b7f9bb2924f6d39c546f
Enrique Corona <enrique_cr1990@hotmail.com>
2018-06-29 00:23:17 -0500

    primer commit de integracion index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <meta name="author" content="Enrique Corona Ricaño">
+    <meta name="description" content="Curriculum Vitae Enrique Corona Ricaño ">
+    <meta name="keywords" content="Curriculum Enrique Corona, Enrique Corona, Licenciado en Informática Enrique Corona, enrique_cr1990@hotmail.com, enriquecr1990@gmail.com">
+
+    <title>Lic. Enrique Corona - CV</title>
+</head>
+<body>
+    <h5>Curriculum Vitae Enrique Corona</h5>
+</body>
+</html>
